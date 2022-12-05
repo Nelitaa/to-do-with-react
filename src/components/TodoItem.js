be38688a4e27abe24010248b1993e05c98069e2e
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-05 19:45:16 -0400

    Adding checkboxes to the Todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,11 @@ class TodoItem extends React.Component {
   render() {
     return (
     <li>
-      <input type="checkbox" /> {this.props.todo.title}
+      <input
+        type="checkbox"
+        checked={this.props.todo.completed}
+        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+      /> {this.props.todo.title}
     </li>
     )
   }
