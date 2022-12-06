2edd4fc0515bbf60973138596667cca47079f12d
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-05 20:06:51 -0400

    Deleting items from the todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,7 +8,11 @@ class TodoItem extends React.Component {
         type="checkbox"
         checked={this.props.todo.completed}
         onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-      /> {this.props.todo.title}
+      /> 
+      {this.props.todo.title}
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        Delete
+      </button>
     </li>
     )
   }
